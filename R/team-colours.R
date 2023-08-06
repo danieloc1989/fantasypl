f6d5a90696aa6fad0d84ba07ea27c29cfec49ad6
Daniel O'Callaghan <danieloc1989@gmail.com>
2023-08-06 22:12:31 +0100

    Added team colours data

---
+++ R/team-colours.R
@@ -0,0 +1,11 @@
+#' Team Colours of the Premier League 23/24
+#'
+#' @format ## `team_colours`
+#' A data frame of the 23/24 Premier League teams:
+#' \describe{
+#'   \item{team_abb}{Team's abbriviation}
+#'   \item{primary_colour}{Team's primary colour}
+#'   \item{secondary_colour}{Team's secondary colour}
+#' }
+#' @source Data are collected from 20 Premier League Teams style guides.
+"team_colours"
